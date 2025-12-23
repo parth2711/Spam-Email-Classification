75ec5eba26d0fbb1bee1252ee98c82b32866850f
parth2711 <64655628+parth2711@users.noreply.github.com>
2025-12-23 15:06:22 +0530

    Finalize UI

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import pickle
 
+st.set_page_config(page_title="Spam Email Classifier",page_icon="📧",layout="centered")
+
 #loading the model and scaler
 with open('model.pkl','rb') as file:
     model=pickle.load(file)
@@ -10,16 +12,37 @@ with open('tfidf.pkl','rb') as file2:
     tfidf=pickle.load(file2)
 
 #proceeding with ui
-st.title('Spam Email Classification')
-
 st.markdown("""
-### About the Model
-This app uses a Support Vector Machine model (Linear Support Vector Classifier) trained on a spam messages dataset.
-""")
+    <h1 style='text-align: center;'>📧 Spam Email Classification</h1>
+    <p style='text-align: center; color: gray;'>
+    Detect if a message is spam or not using SVM.
+    </p>
+    """,unsafe_allow_html=True)
+
+st.divider()
+
+with st.expander("ℹ️ About this app"):
+    st.write(
+        """
+        This app uses a **Linear Support Vector Machine (SVM)** trained on a real world SMS spam dataset.
+
+        Model pipeline:
+
+        -> Text preprocessing
+
+        -> Data Splitting
+
+        -> Vectorization
+
+        -> Fitting Linear Support Vector Classifier
+
+        The model predicts whether a given message is **Spam** or **Not Spam (Ham)**.
+        """
+    )
 
 st.subheader("Enter message received in mail")
 
-message=st.text_input('Message')
+message = st.text_area(label="Paste the email/SMS content below",height=180)
 
 messagetfidf=tfidf.transform([message])
     
@@ -30,5 +53,7 @@ st.subheader("Prediction Result")
 
 if pred=='spam':
     st.error("This message is SPAM") 
+    st.markdown("<p style='color:red;'>Be cautious before clicking links or sharing personal information.</p>",unsafe_allow_html=True)
 else:
     st.success("This message is NOT SPAM (ham)")
+    st.markdown("<p style='color:green;'>This message appears to be safe.</p>",unsafe_allow_html=True)
